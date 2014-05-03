f9a044e893ceed6ba581ce38892d085cabaaa451
hotpepper <heyseth@hotmail.com>
2014-05-02 23:24:28 -0400

    fixed the queries, and added new class

--- Python/altGetStattions.py
+++ Python/altGetStattions.py
@@ -1,33 +1,76 @@
 
 ##alternative work flow
-#import psycopg2
-import pyodbc
+import psycopg2
+
 class get(object):
     def __init__(self):
-        self.OroD = 0 #origin (-1)/destination (1) / NA (0)
         self.lat=0.0 #passed from map
         self.long=0.0#passed from map
-        self.cnxn = psycopg2.connect("dbname=CitiBike user=postgres password = ''")
-        self.resutls =[]
+        self.cnxn = psycopg2.connect("dbname=CitiBike user=postgres password = 'hosts464'")
+        self.results =[]
+        ids=''
         
     def nearby(self, dist):
         #connect to db - pass this query
-        cur = cnxn.cursor()
+        cur = self.cnxn.cursor()
         SQL = """select *
-                from pt_citibikestations_20130731_wavgride as b
-                where st_intersects(ST_BUFFER(ST_GeomFromText('POINT(%s %s)'), %s), b.geom)"""
+                from stations_shp as s
+                where st_intersects(
+                ST_BUFFER(ST_GeomFromText('POINT(%s %s)'), %s), s.geom)"""
         #where st_intersects(ST_BUFFER(ST_GeomFromText('POINT(985069.266642 207601.2461)'), 1000), b.geom)
-        data (self.lat, self.long, dist)
+        data = (self.lat, self.long, dist)
         cur.execute(SQL, data)
-        self.results = cur.fetchall()
+        for i in cur.fetchall():
+            self.results.append(int(i[1]))
+        self.id_format()
+        #self.results = cur.fetchall()
+        
+    def id_format(self):
+        q=str(self.results)
+        self.ids="("+q[1:-1]+")"
 
+class bestBet(object):
+    def __init__(self):
+        self.OroD = 0 #origin (-1)/destination (1) / NA (0)
+        self.top3={}
+        self.dow='FRI'
+        self.hh=8
+        self.fifteen=0
+        self.results ={}
+        
+        
+    def query(self, ids, cnxn):
+        cur = cnxn.cursor()
+        #string concatination here is wrong, but resolved sql error
+        SQL = """select *
+                from summary as s
+                where s.station_id in """+ids+"""
+                and s.dow = %s
+                and s.hh = %s
+                and s."Fifteen" = %s
+                order by s."Bikes" desc"""
+        data = (self.dow, self.hh, self.fifteen)
+        cur.execute(SQL, data)
+        temp = cur.fetchall()
+        
+        for i in temp:
+            self.results[int(i[1])]=i[6]
+        return temp[:3]
+
+    def best(self,ids,cnxn):
+        t3=self.query(ids,cnxn)
+        for i in t3:
+            self.top3[i[1]]=i[6]
+
+        
 #testing----------------
 
 get= get()
-get.lat=985069.266642
-get.long=207601.2461
-get.nearby(1000)
+best=bestBet()
+get.lat=-73.99392888
+get.long=40.76727216
+get.nearby(0.01)
+
+best.best(get.ids,get.cnxn)
         
-
-
 
