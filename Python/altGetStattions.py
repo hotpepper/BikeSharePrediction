20468ca5cd8b5c0e208b483380559423566824a0
unknown <shostetter@DOT-2Z9BTW1.dot.nycnet>
2014-05-05 11:22:26 -0400

    added user input class

--- Python/altGetStattions.py
+++ Python/altGetStattions.py
@@ -1,16 +1,27 @@
 
 ##alternative work flow
 import psycopg2
+from datetime import datetime
+
+class userInput(object):
+    def __init__(self):
+        self.Olat=0.0
+        self.Olong=0.0
+        self.Odatetime=None
+        self.Dlat=0.0
+        self.Dlong=0.0
+        self.Ddatetime=None
+        
+    def __str__(self):
+        return '''gets the start and end locations and times from map'''
 
 class get(object):
     def __init__(self):
-        self.lat=0.0 #passed from map
-        self.long=0.0#passed from map
         self.cnxn = psycopg2.connect("dbname=CitiBike user=postgres password = ''")
         self.results =[]
         ids=''
         
-    def nearby(self, dist):
+    def nearby(self, Ulat,Ulong,dist = 0.01):
         #connect to db - pass this query
         cur = self.cnxn.cursor()
         SQL = """select *
@@ -18,7 +29,7 @@ class get(object):
                 where st_intersects(
                 ST_BUFFER(ST_GeomFromText('POINT(%s %s)'), %s), s.geom)"""
         #where st_intersects(ST_BUFFER(ST_GeomFromText('POINT(985069.266642 207601.2461)'), 1000), b.geom)
-        data = (self.lat, self.long, dist)
+        data = (Ulat, Ulong, dist)
         cur.execute(SQL, data)
         for i in cur.fetchall():
             self.results.append(int(i[1]))
@@ -31,15 +42,31 @@ class get(object):
 
 class bestBet(object):
     def __init__(self):
-        self.OroD = 0 #origin (-1)/destination (1) / NA (0)
         self.top3={}
-        self.dow='FRI'
+        self.dow=''
         self.hh=8
         self.fifteen=0
         self.results ={}
+
+    def getFifteen(self, minute):
+        m=int(minute)
+        if m>53: hself.hh+=1
+        if m <8: f=0
+        elif m<24: f=15
+        elif m<38: f=30
+        elif m<53: f=45
+        else: f=0
+        self.fifteen=f
+      
+    def processDate(self, Udatetime):
+        '''takes in the datetime of the start or end and pulls necessary data'''
+        day = {1:'MON', 2:'TUE', 3:'WED',4:'THUR',5:'FRI',6:'SAT',7:'SUN'}
+        self.dow = day[Udatetime.weekday()]
+        self.hh = Udatetime.hour
+        getFifteen(Udatetime.minute)
         
-        
-    def query(self, ids, cnxn):
+    def query(self, ids, cnxn, Udatetime):
+        processDate(Udatetime)
         cur = cnxn.cursor()
         #string concatination here is wrong, but resolved sql error
         SQL = """select *
@@ -57,20 +84,22 @@ class bestBet(object):
             self.results[int(i[1])]=i[6]
         return temp[:3]
 
-    def best(self,ids,cnxn):
-        t3=self.query(ids,cnxn)
+    def best(self,ids,cnxn, Udatetime):
+        t3=self.query(ids,cnxn, Udatetime)
         for i in t3:
             self.top3[i[1]]=i[6]
 
         
 #testing----------------
+userInput =  userInput()
+userInput.Olat=-73.99392888
+userInput.Olong=40.76727216
+userInput.Odatetime=datetme.datetime.now()
 
 get= get()
 best=bestBet()
-get.lat=-73.99392888
-get.long=40.76727216
-get.nearby(0.01)
-
-best.best(get.ids,get.cnxn)
+get.nearby(userInput.Olat,userInput.Olong)
+ 
+best.best(get.ids,get.cnxn, userInput.Odatetime)
         
 
